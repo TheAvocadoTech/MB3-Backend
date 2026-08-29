ff7ba6659c7b9853a836d131ccc42d58bd5c3f95
SARATH SURESH C <57208715+sarathsureshc@users.noreply.github.com>
2026-08-29 18:21:19 +0530

    Remove DNS settings from db.js
    Removed DNS configuration for MongoDB Atlas SRV lookup.

--- config/db.js
+++ config/db.js
@@ -1,12 +1,7 @@
 const mongoose = require("mongoose");
-const dns = require("dns");
 require("dotenv").config();
 
-// Set Google Public DNS for SRV record resolution (fixes MongoDB Atlas SRV lookup issues)
-dns.setServers(["8.8.8.8", "8.8.4.4"]);
-if (dns.setDefaultResultOrder) {
-  dns.setDefaultResultOrder("ipv4first");
-}
+
 
 const connectDB = async () => {
   try {
